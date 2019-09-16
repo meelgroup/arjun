9e61a4481fdee8b4790346758b3a7f6f3322d309
kuldeepmeel <meel@comp.nus.edu.sg>
2019-09-16 19:17:05 +0800

    Adding forward round

--- src/main.cpp
+++ src/main.cpp
@@ -450,7 +450,6 @@ void add_fixed_clauses()
         dont_elim.push_back(Lit(i+orig_num_vars, false));
     }
 }
-
 void fill_assumptions_one(
     vector<Lit>& assumptions,
     vector<uint32_t>& unknown,
@@ -468,7 +467,6 @@ void fill_assumptions_one(
         assert(ass_var != var_Undef);
         assumptions.push_back(Lit(ass_var, true));
     }
-
     //Add unknown as assumptions, clean "unknown"
     uint32_t j = 0;
     std::sort(unknown.begin(), unknown.end(), IncidenceSorter(incidence));
@@ -528,6 +526,108 @@ void fill_assumptions_inv(
     //clear seen
     for(const auto& x: assumptions) {
         seen[x.var()] = 0;
+    }
+}
+
+void fill_assumptions_forward_round(
+    vector<Lit>& assumptions,
+    const vector<uint32_t>& indep,
+    vector<uint32_t>& unknown,
+    vector<uint32_t>& dep,
+    uint32_t group,
+    uint32_t offs,
+    vector<char>& guess_set
+)
+{
+
+    assumptions.clear();
+    for(auto& x: seen) {
+        assert(x == 0);
+    }
+
+    //Add known independent as assumptions
+    for(const auto& var: indep) {
+        assert(var < orig_num_vars);
+        uint32_t ass = var_to_indic[var];
+        assert(ass != var_Undef);
+        if (!seen[ass]) {
+            seen[ass] = 1;
+            assumptions.push_back(Lit(ass, true));
+        }
+    }
+
+    for(uint32_t i = group*offs; i < group*(offs+1) && i < unknown.size(); i++) {
+        uint32_t var = unknown[i];
+        assert(var < orig_num_vars);
+        if (guess_set[var]) {
+            uint32_t ass = var_to_indic[var];
+            if (!seen[ass]) {
+                seen[ass] = 1;
+                assumptions.push_back(Lit(ass, true));
+            }
+        }
+    }
+
+    for(const auto& var: dep) {
+        assert(var < orig_num_vars);
+
+        uint32_t ass = var_to_indic[var];
+        assert(ass != var_Undef);
+        assert(!seen[ass]);
+        assumptions.push_back(Lit(ass, true));
+    }
+
+    //clear seen
+    for(const auto& var: unknown) {
+        uint32_t ass = var_to_indic[var];
+        seen[ass] = 0;
+    }
+
+    for(const auto& var: indep) {
+        uint32_t ass = var_to_indic[var];
+        seen[ass] = 0;
+    }
+}
+
+void set_guess_forward_round(
+    const vector<uint32_t>& indep,
+    vector<uint32_t>& unknown,
+    vector<char>& unknown_set,
+    uint32_t group,
+    uint32_t offs,
+    vector<char>& guess_set
+)
+{
+    for(auto& x: seen) {
+        x = 0;
+    }
+
+    //Add known independent as assumptions
+    for(const auto& var: indep) {
+        assert(var < orig_num_vars);
+        uint32_t ass = var_to_indic[var];
+        assert(ass != var_Undef);
+        if (!seen[ass]) {
+            seen[ass] = 1;
+        }
+    }
+
+    for(uint32_t i = group*offs; i < group*(offs+1) && i < unknown.size(); i++) {
+        uint32_t var = unknown[i];
+        assert(var < orig_num_vars);
+        if (unknown_set[var]) {
+            unknown_set[var] = 0;
+            uint32_t ass = var_to_indic[var];
+            if (!seen[ass]) {
+                seen[ass] = 1;
+                guess_set[var] = 1;
+            }
+        }
+    }
+
+    //clear seen
+    for(auto& x: seen) {
+        x = 0;
     }
 }
 
@@ -1353,6 +1453,198 @@ void inverse_round(
     << endl;
 }
 
+bool forward_round(
+    uint32_t max_iters = std::numeric_limits<uint32_t>::max(),
+    uint32_t group = 1,
+    bool reverse = false,
+    bool shuffle = false,
+    int offset = 0)
+{
+    for(const auto& x: seen) {
+        assert(x == 0);
+    }
+
+    double start_round_time = cpuTimeTotal();
+    //start with empty independent set
+    vector<uint32_t> indep;
+    vector<uint32_t> dep;
+    //Initially, all of samping_set is unknown
+    vector<uint32_t> unknown;
+    vector<char> unknown_set;
+    unknown_set.resize(orig_num_vars, 0);
+    for(const auto& x: *sampling_set) {
+        unknown.push_back(x);
+        unknown_set[x] = 1;
+    }
+
+    vector<Lit> assumptions;
+    uint32_t iter = 0;
+    uint32_t not_indep = 0;
+
+    double myTime = cpuTime();
+    vector<char> tried_var_already;
+    tried_var_already.resize(orig_num_vars, 0);
+
+    //Calc mod:
+    uint32_t mod = 1;
+    if ((sampling_set->size()) > 20 ) {
+        uint32_t will_do_iters = sampling_set->size();
+        uint32_t want_printed = 30;
+        mod = will_do_iters/want_printed;
+        mod = std::max<int>(mod, 1);
+    }
+    std::sort(unknown.begin(), unknown.end(), IncidenceSorter(incidence));
+    if (reverse) {
+        std::reverse(unknown.begin(), unknown.end());
+    }
+    if (shuffle) {
+        std::random_shuffle(unknown.begin(), unknown.end());
+    }
+    vector<char> guess_set(orig_num_vars, 0);
+
+    vector<uint32_t> pick_possibilities;
+    for(const auto& unk_v: unknown) {
+        if (unknown_set[unk_v]){
+            pick_possibilities.push_back(unk_v);
+        }
+    }
+    std::sort(pick_possibilities.begin(), pick_possibilities.end(), IncidenceSorter(incidence));
+
+    set_guess_forward_round(
+        indep,
+        unknown,
+        unknown_set,
+        group,
+        offset,
+        guess_set
+    );
+
+    cout << "[mis] Start unknown size: " << pick_possibilities.size() << endl;
+    uint32_t ret_false = 0;
+    uint32_t ret_true = 0;
+    uint32_t ret_undef = 0;
+    while(iter < max_iters) {
+        //Select var
+        uint32_t test_var = var_Undef;
+        for(int i = pick_possibilities.size()-1; i>= 0; i--) {
+            uint32_t var = pick_possibilities[i];
+            if (!tried_var_already[var] && unknown_set[var] && !guess_set[var]) {
+                test_var = pick_possibilities[i];
+                break;
+            } else {
+                pick_possibilities.pop_back();
+            }
+        }
+        if (test_var == var_Undef) {
+            break;
+        }
+        assert(test_var < orig_num_vars);
+        assert(unknown_set[test_var] == 1);
+        unknown_set[test_var] = 0;
+        assert(guess_set[test_var] == 0);
+        tried_var_already[test_var] = 1;
+
+        //Assumption filling: with guess_set that is in range + indep
+        assert(test_var != var_Undef);
+        fill_assumptions_forward_round(
+            assumptions,
+            indep,
+            unknown,
+            dep,
+            group,
+            offset,
+            guess_set
+        );
+        assumptions.push_back(Lit(mult_or_invers_var, true));
+
+        assumptions.push_back(Lit(test_var, false));
+        assumptions.push_back(Lit(test_var + orig_num_vars, true));
+
+        solver->set_max_confl(500);
+        solver->set_no_confl_needed();
+
+        lbool ret = l_Undef;
+        ret = solver->solve(&assumptions);
+        if (ret == l_False) {
+            ret_false++;
+        } else if (ret == l_True) {
+            ret_true++;
+        } else if (ret == l_Undef) {
+            ret_undef++;
+        }
+
+        if (ret == l_Undef) {
+            assert(test_var < orig_num_vars);
+            assert(unknown_set[test_var] == 0);
+            unknown_set[test_var] = 1;
+            unknown.push_back(test_var);
+        } else if (ret == l_True) {
+            //Independent
+            indep.push_back(test_var);
+
+        } else if (ret == l_False) {
+            //not independent
+            not_indep++;
+            dep.push_back(test_var);
+            //TODO: In the forward pass, even when the variable is not independent, we can still add it to the assumptions
+        }
+
+        if (iter % mod == (mod-1)) {
+            cout
+            << "[mis] iter: " << std::setw(5) << iter;
+            if (mod == 1) {
+                cout << " mode: "
+                << " guess one "
+                << " ret: " << std::setw(8) << ret;
+            } else {
+                cout
+                << " T/F/U: ";
+                std::stringstream ss;
+                ss << ret_true << "/" << ret_false << "/" << ret_undef;
+                cout << std::setw(10) << std::left << ss.str() << std::right;
+                ret_true = 0;
+                ret_false = 0;
+                ret_undef = 0;
+            }
+            cout
+            << " by: " << std::setw(3) << 1
+            << " U: " << std::setw(7) << unknown.size()
+            << " I: " << std::setw(7) << indep.size()
+            << " N: " << std::setw(7) << not_indep
+            << " T: "
+            << std::setprecision(2) << std::fixed << (cpuTime() - myTime)
+            << endl;
+            myTime = cpuTime();
+
+        }
+        iter++;
+
+        if (iter % 100 == 99) {
+            update_sampling_set(unknown, unknown_set, indep);
+        }
+    }
+
+    for (uint32_t var =0; var < orig_num_vars; var++){
+        if (guess_set[var]){
+            unknown_set[var] = 1;
+            unknown.push_back(var);
+        }
+    }
+    for (auto var: indep) {
+        if (!unknown_set[var]) {
+            unknown.push_back(var);
+            unknown_set[var] = 1;
+        }
+    }
+    indep.clear();
+
+    update_sampling_set(unknown, unknown_set, indep);
+    cout << "[mis] one_round finished T: "
+    << std::setprecision(2) << std::fixed << (cpuTime() - start_round_time)
+    << endl;
+
+    return iter < max_iters;
+}
 void simp(vector<char>* unknown_set)
 {
     if (conf.simp_at_start) {
@@ -1582,9 +1874,14 @@ int main(int argc, char** argv)
     uint32_t round_num = 0;
 
     bool cont = true;
+    bool forward = true;
     while(cont) {
         if (sampling_set->size() > 60) {
             simp();
+        }
+
+        if (conf.guess) {
+            run_guess();
         }
 
 
@@ -1603,13 +1900,19 @@ int main(int argc, char** argv)
 
         cout << "[mis] ===--> Doing a run for " << num << endl;
         if (num == 1) {
-            cont = !one_round(5000);
+            if (forward) {
+                cout << " FORWARD " << endl;
+                uint32_t guess_indep = std::max<uint32_t>(sampling_set->size()/5, 10);
+                forward_round(50000, guess_indep, false, false, 0);
+                cont = true;
+            } else {
+                cout << " BACKWARD " << endl;
+                cont = !one_round(5000);
+            }
+            forward = !forward;
         } else {
             group_round(num);
             cont = true;
-        }
-        if (conf.guess) {
-            run_guess();
         }
         round_num++;
     }
