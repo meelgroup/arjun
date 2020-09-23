6a28e623d6d82a6253f55cbc76ac6886d4faa4e2
Mate Soos <soos.mate@gmail.com>
2020-09-23 21:46:29 +0200

    Allowing changing between smart and dumb duplication

--- src/common.cpp
+++ src/common.cpp
@@ -271,7 +271,11 @@ void Common::init_solver_setup(bool init_sampling, string fname)
     incidence = solver->get_var_incidence();
 
     //Read in file again, with offset
-    duplicate_problem();
+    if (conf.smart_duplicate) {
+        duplicate_problem();
+    } else {
+        readInAFile(fname.c_str(), orig_num_vars, false);
+    }
     solver->set_intree_probe(false);
     solver->set_distill(false);
 
