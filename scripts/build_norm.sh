32197366cc3253c3a9526d22d8aa5606b62beb62
Mate Soos <soos.mate@gmail.com>
2021-01-14 01:23:52 +0100

    Fixing build script

--- scripts/build_norm.sh
+++ scripts/build_norm.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rm -rf lib* Test* tests* include tests CM* cmake* arjun
+rm -rf lib* Test* tests* include tests CM* cmake* arjun Makefile
 cmake -DENABLE_TESTING=ON ..
 make -j6
 make test
