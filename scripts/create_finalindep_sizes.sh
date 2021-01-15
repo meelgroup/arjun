6b2b70ab79460b224af0160a2b5aa0dd53e67576
Mate Soos <soos.mate@gmail.com>
2021-01-15 20:27:27 +0100

    Fixing script

--- scripts/create_finalindep_sizes.sh
+++ scripts/create_finalindep_sizes.sh
@@ -8,6 +8,7 @@ for x in `ls | grep $1`; do
     echo At $x
     pwd
     xzgrep " 0$" *.out_finalindep.xz | awk '{print $1 " " NF-3}' | sed "s/.cnf.gz.no_w.cnf.gz.out_finalindep.xz:c/.cnf.gz/" > final_indeps.csv
+    grep -f unsolved_pre.csv /home/projects/11000744/matesoos/counting2-vars.csv >> final_indeps.csv
     cat final_indeps.csv | awk '{a+=$2;x+=1} END{printf "%7.3f", a/x}' > final_indeps_avg.csv
     );
 done
