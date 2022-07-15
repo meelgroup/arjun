d8731236f881eed3fc73dcb1889582ad9dee6aaf
Mate Soos <soos.mate@gmail.com>
2022-07-15 13:07:13 +0200

    Using less memory in the script

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-for i in {1..20}; do
-    echo $i; a=$((i*10));
-    echo "ulimit -t 3000" > todo-$i.sh;
+for i in {1..8}; do
+    echo $i; a=$((i*25));
+    echo "ulimit -t 5000" > todo-$i.sh;
     echo "ulimit -m 4000000" >> todo-$i.sh;
-    head -n $a todo | tail -n 10 >> todo-$i.sh; chmod +x todo-$i.sh;
+    head -n $a todo | tail -n 25 >> todo-$i.sh; chmod +x todo-$i.sh;
 done
 
 for i in {1..20}; do
-    ./todo-$i.sh > out-$i &
+    ./todo-$i.sh > out-$i 2>&1 &
 done
 wait
