45653601c39ced76e768aa59493f0f2042367664
Mate Soos <soos.mate@gmail.com>
2024-04-24 20:12:30 +0200

    Doing bin-sub before indep calculation, to improve indidence

--- src/simplify.cpp
+++ src/simplify.cpp
@@ -413,7 +413,7 @@ void Minimize::remove_eq_literals() {
     for(auto x: sampling_vars) seen[x] = 1;
     do {
         go_again = false;
-        std::string s("must-scc-vrepl, clean-cls");
+        std::string s("must-scc-vrepl, clean-cls, sub-impl");
         solver->simplify(nullptr, &s);
 
         // [ replaced, replaced_with ]
