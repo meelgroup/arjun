c4b30d87374a93373b4629134db56ec98c83c296
Mate Soos <soos.mate@gmail.com>
2024-04-28 17:22:24 -0400

    Fixing printing

--- src/constants.h
+++ src/constants.h
@@ -50,14 +50,14 @@ using std::setw;
 #define verb_print(a, x) \
     do { \
         if (conf.verb >= a) {\
-            std::cout << COLDEF << "c " << x << COLDEF << std::endl;}\
+            std::cout << "c o " << COLDEF << x << COLDEF << std::endl;}\
     } while (0)
 
 
 #define verb_print2(a, x) \
     do { \
         if (arjdata->conf.verb >= a) {\
-            std::cout << COLDEF << "c " << x << COLDEF << std::endl;}\
+            std::cout << "c o " << COLDEF << x << COLDEF << std::endl;}\
     } while (0)
 
 #if defined(_MSC_VER)
