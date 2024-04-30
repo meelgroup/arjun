b25c0b883bcb51b3389239e8b7d884ca7d7d147f
Mate Soos <soos.mate@gmail.com>
2024-04-30 17:50:42 -0400

    This is NOT correct under normal circumstances, it's a hack

--- src/main.cpp
+++ src/main.cpp
@@ -309,7 +309,7 @@ void do_synthesis() {
     simp_conf.bve_too_large_resolvent = -1;
     cnf = arjun->only_get_simplified_cnf(cnf, simp_conf);
     if (do_revbce) arjun->only_reverse_bce(cnf);
-    if (do_minim_indep) arjun->only_run_minimize_indep_synth(cnf);
+    if (false && do_minim_indep) arjun->only_run_minimize_indep_synth(cnf);
 
     write_synth(cnf, elimtofile, false);
 }
