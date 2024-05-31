34a18f9289cc7c4a493138e126dab2f7cf7d57be
Mate Soos <soos.mate@gmail.com>
2024-05-31 00:43:11 -0400

    Better script

--- scripts/debug-counts.sh
+++ scripts/debug-counts.sh
@@ -6,8 +6,9 @@ rm -f out
 rm -f tmp
 rm -f tmp-renum
 
-./arjun --debugminim "tmp" --verb 5 "$f" out > verb
+./arjun --extend 1 --debugminim "tmp" --verb 5 "$f" out > verb
 grep "w-debug" verb
 ../../ganak/build/ganak --td 0 --arjun 0 --verb 0 "$f"
 ../../ganak/build/ganak --td 0 --arjun 0 --verb 0 tmp
 ../../ganak/build/ganak --td 0 --arjun 0 --verb 0 out
+echo "!!!!!!!!!!!!! NOTE: extend is 0!!!!!!!!!!"
