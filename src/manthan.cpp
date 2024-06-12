01c795c27a6b1777aa9305f5664c5f4cb0df1b0a
Mate Soos <soos.mate@gmail.com>
2024-06-12 14:47:34 -0400

    Manthan

---
+++ src/manthan.cpp
@@ -0,0 +1,39 @@
+/*
+ Arjun
+
+ Copyright (c) 2020, Mate Soos and Kuldeep S. Meel. All rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+#include "manthan.h"
+
+using namespace ArjunInt;
+using namespace ArjunNS;
+
+void Manthan::do_manthan(SimplifiedCNF& cnf) {
+    // Grand master plan
+    // 1. Get 10k samples
+    // 2. Run boost to get a tree one-by-one and thereby generate an order
+    // 3. Find counterexample
+    // 4. Make counterexample as close to being good as possible. Originally maxsat, but we can try greedy
+    // 5a -- we could fix solutions one-by-one but that's slow
+    // 5b -- instead, get the conflict from the assumptions, which is a kind of poor "core",
+    //       and do the "stupid" fix on that.
+}
