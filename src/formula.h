a63c31e2d8024a5a09856adf11cf54be5dc3761d
Mate Soos <soos.mate@gmail.com>
2024-06-19 23:50:59 -0400

    Formula and/or

--- src/formula.h
+++ src/formula.h
@@ -21,7 +21,6 @@ struct FormulaHolder {
         // TODO: we could have a flag of what has already been inserted into
         // solver_train
         std::vector<std::vector<CMSat::Lit>> clauses;
-        std::set<uint32_t> inter_vs;
         CMSat::Lit out = CMSat::lit_Error; // member of inter_vs
     };
 
@@ -29,24 +28,46 @@ struct FormulaHolder {
     Formula constant_formula(int value) {
         Formula ret;
         ret.out = value ? my_true_lit : ~my_true_lit;
-        ret.inter_vs.insert(my_true_lit.var());
         return ret;
     }
 
 
     Formula compose_ite(const Formula& fleft, const Formula& fright, const Formula& branch) {
         Formula ret = compose_ite(fleft, fright, branch.out);
-        ret.inter_vs.insert(branch.inter_vs.begin(), branch.inter_vs.end());
-        ret.inter_vs.insert(branch.out.var());
-        ret.inter_vs.insert(ret.out.var());
         ret.clauses.insert(ret.clauses.end(), branch.clauses.begin(), branch.clauses.end());
+        return ret;
+    }
+
+    Formula neg(const Formula& f) {
+        Formula ret = f;
+        ret.out = ~f.out;
+        return ret;
+    }
+
+    Formula compose_and(const Formula& fleft, const Formula& fright) {
+        return neg(compose_or(neg(fleft), neg(fright)));
+    }
+
+    Formula compose_or(const Formula& fleft, const Formula& fright) {
+        Formula ret;
+        ret.clauses = fleft.clauses;
+        for(const auto& cl: fright.clauses) ret.clauses.push_back(cl);
+
+        solver->new_var();
+        uint32_t fresh_v = solver->nVars()-1;
+        Lit fl = Lit(fresh_v, false);
+
+        ret.clauses.push_back({~fl, fleft.out, fright.out});
+        ret.clauses.push_back({fl, ~fleft.out});
+        ret.clauses.push_back({fl, ~fright.out});
+
+        ret.out = fl;
+
         return ret;
     }
 
     Formula compose_ite(const Formula& fleft, const Formula& fright, Lit branch) {
         Formula ret;
-        ret.inter_vs = fleft.inter_vs;
-        for(const auto& v: fright.inter_vs) ret.inter_vs.insert(v);
         ret.clauses = fleft.clauses;
         for(const auto& cl: fright.clauses) ret.clauses.push_back(cl);
         solver->new_var();
@@ -66,9 +87,6 @@ struct FormulaHolder {
         Lit l = fleft.out;
         Lit r = fright.out;
         Lit fresh = Lit(fresh_v, false);
-        ret.inter_vs.insert(fresh_v);
-        ret.inter_vs.insert(l.var());
-        ret.inter_vs.insert(r.var());
         ret.clauses.push_back({~b, fresh, ~l});
         ret.clauses.push_back({~b, ~fresh, l});
         ret.clauses.push_back({b, fresh, ~r});
@@ -90,10 +108,6 @@ inline std::ostream& operator<<(std::ostream& os, const FormulaHolder::Formula& 
             os << std::setw(6) << l;
         }
         cout << " 0" << endl;
-    }
-    os << "Intermediates: ";
-    for (const auto& v : f.inter_vs) {
-        os << v+1 << " ";
     }
     os << endl;
     os << "Output: " << f.out;
