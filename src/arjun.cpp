ab2651b8f9fa3846816029cec271f25120db7509
Mate Soos <soos.mate@gmail.com>
2024-06-26 20:21:54 -0400

    Let's try this

--- src/arjun.cpp
+++ src/arjun.cpp
@@ -230,6 +230,13 @@ void Arjun::elim_to_file(SimplifiedCNF& cnf, bool all_indep,
     cnf.remove_equiv_weights();
     cnf = only_get_simplified_cnf(cnf, simp_conf);
     cnf.remove_equiv_weights();
+    auto simp_conf2 = simp_conf;
+    simp_conf2.bve_grow_iter1 = 0;
+    simp_conf2.bve_grow_iter2 = 0;
+    simp_conf2.iter1 = 1;
+    simp_conf2.iter2 = 1;
+    simp_conf2.bve_too_large_resolvent = 4;
+    cnf = only_get_simplified_cnf(cnf, simp_conf2);
     if (sbva_steps)
         only_run_sbva(cnf, sbva_steps,
                 sbva_cls_cutoff, sbva_lits_cutoff, sbva_tiebreak);
