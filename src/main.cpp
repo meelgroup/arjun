939be08013412469f40af726fbfa9423f0018a6a
Mate Soos <soos.mate@gmail.com>
2024-09-13 12:19:27 +0200

    Cleanup of the options

--- src/main.cpp
+++ src/main.cpp
@@ -34,12 +34,23 @@
 #include <zlib.h>
 #endif
 #include "argparse.hpp"
+#include <cryptominisat5/dimacsparser.h>
 
 #include "time_mem.h"
 #include "arjun.h"
 #include "config.h"
 #include "helper.h"
-#include <cryptominisat5/dimacsparser.h>
+
+#define myopt(name, var, fun, hhelp) \
+    program.add_argument(name) \
+        .action([&](const auto& a) {var = std::fun(a.c_str());}) \
+        .default_value(var) \
+        .help(hhelp)
+#define myopt2(name1, name2, var, fun, hhelp) \
+    program.add_argument(name1, name2) \
+        .action([&](const auto& a) {var = std::fun(a.c_str());}) \
+        .default_value(var) \
+        .help(hhelp)
 
 using std::cout;
 using std::endl;
@@ -55,8 +66,8 @@ string input_file;
 string elimtofile;
 
 SimpConf simp_conf;
-int renumber = true;
-int gates = 1;
+int do_renumber = true;
+int do_gates = 1;
 int do_extend_indep = 1;
 int redundant_cls = true;
 int simptofile = true;
@@ -77,174 +88,59 @@ int do_minim_indep = true;
 bool all_indep = false;
 string debug_minim;
 int do_pre_manthan = false;
-int num_samples = 2000;
 double cms_mult = -1.0;
+
+string print_version() {
+    std::stringstream ss;
+    ss << "c o Arjun Version: " << arjun->get_version_info() << endl;
+    ss << arjun->get_solver_version_info();
+    ss << "c o [arjun] Compilation environment: " << arjun->get_compilation_env() << endl;
+    return ss.str();
+}
 
 void add_arjun_options()
 {
     conf.verb = 1;
-    program.add_argument("-v", "--verb")
-        .action([&](const auto& a) {conf.verb = std::atoi(a.c_str());})
-        .default_value(conf.verb)
-        .help("verbosity");
-    program.add_argument("--allindep")
-        .action([&](const auto& a) {all_indep = std::atoi(a.c_str());})
-        .default_value(all_indep)
-        .help("All variables are in the independent set. The indep set is given only to help Arjun");
-    program.add_argument("--premanthan")
-        .action([&](const auto& a) {do_pre_manthan = std::atoi(a.c_str());})
-        .default_value(do_pre_manthan)
-        .help("All variables are in the independent set. The indep set is given only to help Arjun");
-    program.add_argument("--maxc")
-        .action([&](const auto& a) {conf.backw_max_confl = std::atoi(a.c_str());})
-        .default_value(conf.backw_max_confl)
-        .help("Maximum conflicts per variable in backward mode");
-    program.add_argument("--extend")
-        .action([&](const auto& a) {do_extend_indep = std::atoi(a.c_str());})
-        .default_value(do_extend_indep)
-        .help("Extend independent set just before CNF dumping");
+    myopt2("-v", "--verb", conf.verb, atoi, "Verbosity");
+    program.add_argument("-v", "--version") \
+        .action([&](const auto&) {print_version(); exit(0);}) \
+        .flag()
+        .help("Print version and exit");
+
+    myopt("--allindep", all_indep , atoi, "Ignore indep, set all vars to independent set.");
+    myopt("--premanthan", do_pre_manthan, atoi, "Run all simplifcation before Manthan");
+    myopt("--maxc", conf.backw_max_confl, atoi,"Maximum conflicts per variable in backward mode");
+    myopt("--extend", do_extend_indep, atoi,"Extend independent set just before CNF dumping");
     program.add_argument("--synth")
         .action([&](const auto&) {synthesis = 1;})
         .default_value(synthesis)
         .flag()
-        .help("Define for synthesis");
-    program.add_argument("--unate")
-        .action([&](const auto& a) {do_unate = std::atoi(a.c_str());})
-        .default_value(do_unate)
-        .help("Perform unate analysis");
-    program.add_argument("--backbone")
-        .action([&](const auto& a) {do_pre_backbone = std::atoi(a.c_str());})
-        .default_value(do_pre_backbone)
-        .help("Perform backbone analysis");
-    program.add_argument("--synthbve")
-        .action([&](const auto& a) {do_synth_bve = std::atoi(a.c_str());})
-        .default_value(do_synth_bve)
-        .help("Perform BVE for synthesis");
-    program.add_argument("--revbce")
-        .action([&](const auto& a) {do_revbce = std::atoi(a.c_str());})
-        .default_value(do_revbce)
-        .help("Perform reverse BCE");
-    program.add_argument("--sbva")
-        .action([&](const auto& a) {sbva_steps = std::atoi(a.c_str());})
-        .default_value(sbva_steps)
-        .help("SBVA timeout. 0 = no sbva");
-    program.add_argument("--prebackbone")
-        .action([&](const auto& a) {do_pre_backbone = std::atoi(a.c_str());})
-        .default_value(do_pre_backbone)
-        .help("Perform backbone before other things");
-    program.add_argument("--debugsynt")
-        .action([&](const auto&) {debug_synt = true;})
-        .flag()
-        .help("Debug synthesis");
-    program.add_argument("--samples")
-        .action([&](const auto& a) {num_samples = std::atoi(a.c_str());})
-        .help("Number of samples");
-    program.add_argument("--sbvaclcut")
-        .action([&](const auto& a) {sbva_cls_cutoff = std::atoi(a.c_str());})
-        .default_value(sbva_cls_cutoff)
-        .help("SBVA heuristic cutoff. The higher, the more it needs to improve to be applied");
-    program.add_argument("--sbvalitcut")
-        .action([&](const auto& a) {sbva_lits_cutoff = std::atoi(a.c_str());})
-        .default_value(sbva_lits_cutoff)
-        .help("SBVA heuristic cutoff. The higher, the more it needs to improve to be applied");
-    program.add_argument("--findbins")
-        .action([&](const auto& a) {conf.oracle_find_bins = std::atoi(a.c_str());})
-        .default_value(conf.oracle_find_bins)
-        .help("How aggressively find binaries via oracle");
-    program.add_argument("--probe")
-        .action([&](const auto& a) {conf.probe_based = std::atoi(a.c_str());})
-        .default_value(conf.probe_based)
-        .help("Do probing during orignal Arjun");
-    program.add_argument("--sbvabreak")
-        .action([&](const auto& a) {
-                sbva_tiebreak = std::atoi(a.c_str());
-                if (sbva_tiebreak != 0 && sbva_tiebreak != 1 ) {
-                    cout << "Unrecognized tie break: sbva/bva allowed." << endl;
-                    exit(-1);}
-                })
-        .default_value(1)
-        .help("SBVA tie break: sbva or bva");
+        .help("Run synthesis");
+    myopt("--debugsynt", debug_synt, atoi, "Debug synthesis");
+    myopt("--unate", do_unate, atoi,"Perform unate analysis");
+    myopt("--backbone", do_pre_backbone, atoi,"Perform backbone analysis");
+    myopt("--synthbve", do_synth_bve, atoi,"Perform BVE for synthesis");
+    myopt("--revbce", do_revbce, atoi,"Perform reverse BCE");
+    myopt("--sbva", sbva_steps, atoi,"SBVA timeout. 0 = no sbva");
+    myopt("--prebackbone", do_pre_backbone, atoi,"Perform backbone before other things");
+    myopt("--samples", conf.num_samples, atoi,"Number of samples");
 
-    /* po::options_description simp_options("Simplification before indep detection"); */
-    /* simp_options.add_options() */
-    program.add_argument("--bvepresimp")
-        .action([&](const auto& a) {conf.bve_pre_simplify = std::atoi(a.c_str());})
-        .default_value(conf.bve_pre_simplify)
-        .help("simplify");
-    program.add_argument("--simp")
-        .action([&](const auto& a) {conf.simp = std::atoi(a.c_str());})
-        .default_value(conf.simp)
-        .help("Do ANY sort of simplification");
-    program.add_argument("--probe")
-        .action([&](const auto& a) {conf.probe_based = std::atoi(a.c_str());})
-        .default_value(conf.probe_based)
-        .help("Use simple probing to set (and define) some variables");
-    program.add_argument("--intree")
-        .action([&](const auto& a) {conf.intree = std::atoi(a.c_str());})
-        .default_value(conf.intree)
-        .help("intree");
+    // Simplification options for minim
+    myopt("--probe", conf.probe_based, atoi,"Do probing during orignal Arjun");
+    myopt("--bvepresimp", conf.bve_pre_simplify, atoi,"simplify");
+    myopt("--simp", conf.simp, atoi,"Do ~ sort of simplification during indep minimixation");
+    myopt("--probe", conf.probe_based, atoi,"Use simple probing to set (and define) some variables");
+    myopt("--intree", conf.intree, atoi,"intree");
 
-    /* po::options_description gate_options("Gate options"); */
-    /* gate_options.add_options() */
-    program.add_argument("--gates")
-        .action([&](const auto& a) {gates = std::atoi(a.c_str());})
-        .default_value(1)
-        .help("Turn on/off all gate-based definability");
-    program.add_argument("--nogatebelow")
-        .action([&](const auto& a) {conf.no_gates_below = std::atof(a.c_str());})
-        .default_value(conf.no_gates_below)
-        .help("Don't use gates below this incidence relative position (1.0-0.0) to minimize the independent set. Gates are not very accurate, but can save a LOT of time. We use them to get rid of most of the uppert part of the sampling set only. Default is 99% is free-for-all, the last 1% we test. At 1.0 we test everything, at 0.0 we try using gates for everything.");
-    program.add_argument("--orgate")
-        .action([&](const auto& a) {conf.or_gate_based = std::atoi(a.c_str());})
-        .default_value(conf.or_gate_based)
-        .help("Use 3-long gate detection in SAT solver to define some variables");
-    program.add_argument("--irreggate")
-        .action([&](const auto& a) {conf.irreg_gate_based = std::atoi(a.c_str());})
-        .default_value(conf.irreg_gate_based)
-        .help("Use irregular gate based removal of variables from sampling set");
-    program.add_argument("--itegate")
-        .action([&](const auto& a) {conf.ite_gate_based = std::atoi(a.c_str());})
-        .default_value(conf.ite_gate_based)
-        .help("Use ITE gate detection in SAT solver to define some variables");
-    program.add_argument("--xorgate")
-        .action([&](const auto& a) {conf.xor_gates_based = std::atoi(a.c_str());})
-        .default_value(conf.xor_gates_based)
-        .help("Use XOR detection in SAT solver to define some variables");
+    // Gate options
+    myopt("--gates", do_gates, atoi,"Turn on/off all gate-based definability");
+    myopt("--nogatebelow", conf.no_gates_below, atof,"Don't use gates below this incidence relative position (1.0-0.0) to minimize the independent set. Gates are not very accurate, but can save a LOT of time. We use them to get rid of most of the uppert part of the sampling set only. Default is 99% is free-for-all, the last 1% we test. At 1.0 we test everything, at 0.0 we try using gates for everything.");
+    myopt("--orgate", conf.or_gate_based, atoi,"Use 3-long gate detection in SAT solver to define variables");
+    myopt("--irreggate", conf.irreg_gate_based, atoi,"Use irregular gate-based removal of vars from indep set");
+    myopt("--itegate", conf.ite_gate_based, atoi,"Use ITE gate detection in SAT solver to define some variables");
+    myopt("--xorgate", conf.xor_gates_based, atoi,"Use XOR detection in SAT solver to define some variables");
 
-    /* po::options_description debug_options("Debug options"); */
-    /* debug_options.add_options() */
-    program.add_argument("--minimize")
-        .action([&](const auto& a) {do_minim_indep = std::atoi(a.c_str());})
-        .default_value(do_minim_indep)
-        .help("Minimize indep set");
-    program.add_argument("--gaussj")
-        .action([&](const auto& a) {conf.gauss_jordan = std::atoi(a.c_str());})
-        .default_value(conf.gauss_jordan)
-        .help("Use XOR finding and Gauss-Jordan elimination");
-    program.add_argument("--iter1")
-        .action([&](const auto& a) {simp_conf.iter1 = std::atoi(a.c_str());})
-        .default_value(simp_conf.iter1)
-        .help("Puura iterations before oracle");
-    program.add_argument("--iter1grow")
-        .action([&](const auto& a) {simp_conf.bve_grow_iter1 = std::atoi(a.c_str());})
-        .default_value(simp_conf.bve_grow_iter1)
-        .help("Puura BVE grow rate allowed before Oracle");
-    program.add_argument("--iter2")
-        .action([&](const auto& a) {simp_conf.iter2 = std::atoi(a.c_str());})
-        .default_value(simp_conf.iter2)
-        .help("Puura iterations after oracle");
-    program.add_argument("--iter2grow")
-        .action([&](const auto& a) {simp_conf.bve_grow_iter2 = std::atoi(a.c_str());})
-        .default_value(simp_conf.bve_grow_iter2)
-        .help("Puura BVE grow rate allowed after Oracle");
-    program.add_argument("--bveresolvmaxsz")
-        .action([&](const auto& a) {simp_conf.bve_too_large_resolvent = std::atoi(a.c_str());})
-        .default_value(simp_conf.bve_too_large_resolvent)
-        .help("Puura BVE max resolvent size in literals. -1 == no limit");
-    program.add_argument("--oraclesparsify")
-        .action([&](const auto& a) {simp_conf.oracle_sparsify = std::atoi(a.c_str());})
-        .default_value(simp_conf.oracle_sparsify)
-        .help("Use Oracle to sparsify");
+    // AppMC
     program.add_argument("--appmc")
         .action([&](const auto&) {
                 simp_conf.appmc = true;
@@ -255,43 +151,32 @@ void add_arjun_options()
                 simp_conf.iter2 = 0;
                 })
         .flag()
-        .help("Set CNF options for appmc");
-    program.add_argument("--oraclevivif")
-        .action([&](const auto& a) {simp_conf.oracle_vivify = std::atoi(a.c_str());})
-        .default_value(simp_conf.oracle_vivify)
-        .help("Use oracle to vivify");
-    program.add_argument("--oraclevivifgetl")
-        .action([&](const auto& a) {simp_conf.oracle_vivify_get_learnts = std::atoi(a.c_str());})
-        .default_value(simp_conf.oracle_vivify_get_learnts)
-        .help("Use oracle to vivify get learnts");
-    program.add_argument("--renumber")
-        .action([&](const auto& a) {renumber = std::atoi(a.c_str());})
-        .default_value(renumber)
-        .help("Renumber variables to start from 1...N in CNF. Setting this to 0 is EXPERIMENTAL!!");
-    program.add_argument("--distill")
-        .action([&](const auto& a) {conf.distill = std::atoi(a.c_str());})
-        .default_value(conf.distill);
-    program.add_argument("--bce")
-        .action([&](const auto& a) {do_bce = std::atoi(a.c_str());})
-        .default_value(do_bce)
-        .help("Use blocked clause elimination (BCE). VERY experimental!!");
-    program.add_argument("--red")
-        .action([&](const auto& a) {redundant_cls = std::atoi(a.c_str());})
-        .default_value(redundant_cls)
-        .help("Also dump redundant clauses");
-    program.add_argument("--specifiedorder")
-        .action([&](const auto& a) {conf.specified_order_fname = a;})
-        .default_value(conf.specified_order_fname)
-        .help("Try to remove variables from the independent set in this order. "
-                "File must contain a variable on each line. "
-                "Variables start at ZERO. Variable from the BOTTOM will be removed FIRST. This is for DEBUG ONLY");
-    program.add_argument("--debugminim")
-        .action([&](const auto& a) {debug_minim = a;})
-        .help("Create this file that is the CNF after indep set minimization");
-    program.add_argument("--cmsmult")
-        .action([&](const auto& a) {conf.cms_mult = std::atof(a.c_str());})
-        .default_value(conf.cms_mult)
-        .help("Multiply timeouts in CMS by this. Default is -1, which means no change");
+        .help("Set CNF simplification options for appmc");
+
+    // Detailed Configuration
+    myopt("--sbvaclcut", sbva_cls_cutoff, atoi,"SBVA heuristic cutoff. Higher -> only appied to more clauses");
+    myopt("--sbvalitcut", sbva_lits_cutoff, atoi,"SBVA heuristic cutoff. Higher -> only appied to larger clauses");
+    myopt("--findbins", conf.oracle_find_bins, atoi,"How aggressively find binaries via oracle");
+    myopt("--sbvabreak",  sbva_tiebreak, atoi,"SBVA tie break: 1=sbva or 0=bva");
+    myopt("--gaussj", conf.gauss_jordan, atoi,"Use XOR finding and Gauss-Jordan elimination");
+    myopt("--iter1", simp_conf.iter1, atoi,"Puura iterations before oracle");
+    myopt("--iter1grow", simp_conf.bve_grow_iter1, atoi,"Puura BVE grow rate allowed before Oracle");
+    myopt("--iter2", simp_conf.iter2, atoi,"Puura iterations after oracle");
+    myopt("--iter2grow", simp_conf.bve_grow_iter2, atoi,"Puura BVE grow rate allowed after Oracle");
+    myopt("--bveresolvmaxsz", simp_conf.bve_too_large_resolvent, atoi,"Puura BVE max resolvent size in literals. -1 == no limit");
+    myopt("--oraclesparsify", simp_conf.oracle_sparsify, atoi,"Use Oracle to sparsify");
+    myopt("--oraclevivif", simp_conf.oracle_vivify, atoi,"Use oracle to vivify");
+    myopt("--oraclevivifgetl", simp_conf.oracle_vivify_get_learnts, atoi,"Use oracle to vivify get learnts");
+    myopt("--distill", conf.distill, atoi, "Distill clauses before minimization of indep");
+    myopt("--bce", do_bce, atoi, "Use blocked clause elimination (BCE) statically");
+    myopt("--red", redundant_cls, atoi,"Also dump redundant clauses");
+
+    // Debug
+    myopt("--renumber", do_renumber, atoi,"Renumber variables to start from 1...N in CNF.");
+    myopt("--specifiedorder", conf.specified_order_fname, string, "Try to remove variables from the independent set in this order. File must contain a variable on each line. Variables start at ZERO. Variable from the BOTTOM will be removed FIRST. This is for DEBUG ONLY");
+    myopt("--minimize", do_minim_indep, atoi,"Minimize indep set");
+    myopt("--debugminim", debug_minim, string,"Create this file that is the CNF after indep set minimization");
+    myopt("--cmsmult", conf.cms_mult, atof,"Multiply timeouts in CMS by this. Default is -1, which means no change. Useful for debugging");
 
     program.add_argument("files").remaining().help("input file and output file");
 }
@@ -319,7 +204,7 @@ void set_config(ArjunNS::Arjun* arj) {
     arj->set_intree(conf.intree);
     arj->set_bve_pre_simplify(conf.bve_pre_simplify);
     arj->set_cms_mult(conf.cms_mult);
-    if (gates) {
+    if (do_gates) {
       arj->set_or_gate_based(conf.or_gate_based);
       arj->set_ite_gate_based(conf.ite_gate_based);
       arj->set_xor_gates_based(conf.xor_gates_based);
@@ -336,14 +221,13 @@ void set_config(ArjunNS::Arjun* arj) {
     arj->set_backw_max_confl(conf.backw_max_confl);
     arj->set_gauss_jordan(conf.gauss_jordan);
     arj->set_simp(conf.simp);
-    arj->set_num_samples(num_samples);
+    arj->set_num_samples(conf.num_samples);
     arj->set_extend_max_confl(conf.extend_max_confl);
     arj->set_backbone_only_optindep(conf.backbone_only_optindep);
     arj->set_oracle_find_bins(conf.oracle_find_bins);
 }
 
 void do_synthesis() {
-    /* assert(!elimtofile.empty()); */
     SimplifiedCNF cnf;
     read_in_a_file(input_file, &cnf, all_indep);
     if (cnf.get_projected()) cnf.clear_weights_for_nonprojected_vars();
@@ -435,10 +319,13 @@ int main(int argc, char** argv) {
         exit(-1);
     }
 
-    if (conf.verb || program["version"] == true) {
-        cout << "c o Arjun Version: " << arjun->get_version_info() << endl;
-        cout << arjun->get_solver_version_info();
-        cout << "c o [arjun] Compilation environment: " << arjun->get_compilation_env() << endl;
+    if (sbva_tiebreak != 0 && sbva_tiebreak != 1) {
+        cout << "Unrecognized tie break: sbva/bva allowed." << endl;
+        exit(-1);
+    }
+
+    if (conf.verb) {
+        cout << print_version() << endl;
         cout << "c o executed with command line: " << command_line << endl;
     }
     if (program["version"] == true) exit(0);
