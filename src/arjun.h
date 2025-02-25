f64a738beeb4c110773b378c237ca4e517bba3d3
Mate Soos <soos.mate@gmail.com>
2025-02-25 17:24:59 +0100

    Fixing debug printing

--- src/arjun.h
+++ src/arjun.h
@@ -607,7 +607,8 @@ namespace ArjunNS {
             std::set<uint32_t> sampling_vars_set(new_sampl_vars.begin(), new_sampl_vars.end());
             std::set<uint32_t> opt_sampling_vars_set(opt_sampl_vars.begin(), opt_sampl_vars.end());
             bool debug_w = false;
-            std::cout << __FUNCTION__ << " [w-debug] orig multiplier_weight: " << multiplier_weight
+            if (debug_w)
+                std::cout << __FUNCTION__ << " [w-debug] orig multiplier_weight: " << multiplier_weight
                 << std::endl;
 
             // Take units
