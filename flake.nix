991ef807141deaa6d321d6c4036b75aa1cbf62f4
Mate Soos <soos.mate@gmail.com>
2025-05-11 11:21:43 -0400

    Update nix

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     cryptominisat = {
-      url = "github:msoos/cryptominisat/synthesis";
+      url = "github:msoos/cryptominisat/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sbva = {
