45c33d853da8a15c4ad99286bb562fb19c4dda3c
Mate Soos <soos.mate@gmail.com>
2026-01-11 16:42:09 +0100

    Better printing

--- src/arjun.cpp
+++ src/arjun.cpp
@@ -93,7 +93,9 @@ DLL_PUBLIC string Arjun::get_version_sha1() {
 DLL_PUBLIC string Arjun::get_thanks_info(const char* prefix) {
     stringstream ss;
     ss << prefix << "Using ideas by JM Lagniez, and Pierre Marquis" << endl;
-    ss << prefix << "    from paper 'Improving Model Counting [..] IJCAI 2016";
+    ss << prefix << "    from paper: Improving Model Counting [..] IJCAI 2016";
+    ss << prefix << "Using ideas by P. Golia, S. Roy, and K. Meel" << endl;
+    ss << prefix << "    from paper: Manthan: A Data-Driven Approach for Boolean Functional Synthesis" << endl;
     return ss.str();
 }
 
