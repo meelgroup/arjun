4cc007046df6f8dfd996d5dfbe4741488cdddde0
Mate Soos <soos.mate@gmail.com>
2026-01-16 10:52:14 +0100

    Build master branches, and better error reporting when multiple `c p show` is given (#31)
    * Also master branches
    
    * Better multi-c-p-show error reporting thanks to @friguzzi

--- src/arjun.h
+++ src/arjun.h
@@ -935,8 +935,14 @@ struct SimplifiedCNF {
     void set_sampl_vars(const T& vars, bool ignore = false) {
         for(const auto& v: vars) check_var(v);
         if (!ignore) {
+            if (sampl_vars_set) {
+              std::cout << "ERROR: Sampling variables have already been set, you can't set them twice!"
+                << std::endl;
+              std::cout << "       Maybe you have two 'c p show' lines in your file?" << std::endl;
+              exit(EXIT_FAILURE);
+            }
+            assert(!sampl_vars_set && "Sampling variables have already been set!");
             assert(sampl_vars.empty());
-            assert(sampl_vars_set == false);
         }
         sampl_vars.clear();
         sampl_vars_set = true;
