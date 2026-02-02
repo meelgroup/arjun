d00ea730903775bcd93c0b8625e08e399120ff28
Mate Soos <soos.mate@gmail.com>
2026-02-02 21:11:16 +0100

    Add comment

--- src/manthan.cpp
+++ src/manthan.cpp
@@ -78,7 +78,7 @@ vector<int> lits_to_ints(const vector<Lit>& lits) {
 }
 
 // good: benchmarks-qdimacs/small-bug1-fixpoint-10.qdimacs.cnf
-// also good: benchmarks-qdimacs/amba2f9n.sat.qdimacs.cnf
+// also good (some repair): benchmarks-qdimacs/amba2f9n.sat.qdimacs.cnf
 // slow (actually, correct on 1st try at 4e7a4cea5b8a994044466578751ff229b514e747 with --bve 0 --ctxsolver 1 --samples 1000):
 //     benchmarks-qdimacs/bobsmcodic_all_bit_differing_from_cycle.qdimacs.cnf
 // LOTS of repairs, fast, but does not terminate: benchmarks-qdimacs/query01_query42_1344n.qdimacs.cnf
