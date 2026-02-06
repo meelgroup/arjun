9deb068c99dba67a4eb8e37f2159987792cdb6c4
Mate Soos <soos.mate@gmail.com>
2026-02-06 21:42:50 +0100

    Initial autarky

---
+++ src/autarky.cpp
@@ -0,0 +1,132 @@
+/*
+ Arjun
+
+ Copyright (c) 2020, Mate Soos and Kuldeep S. Meel. All rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+#include "autarky.h"
+#include <cryptominisat5/cryptominisat.h>
+#include "time_mem.h"
+#include "arjun.h"
+#include "constants.h"
+
+using namespace ArjunNS;
+using namespace CMSat;
+using std::vector;
+
+Autarky::Autarky(const Config& _conf) : conf(_conf) {}
+
+
+// Following paper https://sun.iwu.edu/~mliffito/publications/sat08_liffiton_autarkies.pdf
+// "Searching for Autarkies to Trim Unsatisfiable Clause Sets"
+void Autarky::do_autarky(SimplifiedCNF& cnf)
+{
+    double start_time = cpuTime();
+    s.set_verbosity(conf.verb);
+    vector<LitSub> lit_sub(cnf.nVars());
+    for(uint32_t i = 0; i < cnf.nVars(); i++) {
+        LitSub ls;
+        s.new_var();
+        ls.pos = Lit(s.nVars()-1, false);
+        s.new_var();
+        ls.neg = Lit(s.nVars()-1, false);
+        lit_sub[i] = ls;
+    }
+
+    vector<Lit> cl_sel(cnf.get_clauses().size());
+    for(uint32_t i = 0; i < cnf.get_clauses().size(); i++) {
+        s.new_var();
+        cl_sel[i] = Lit(cnf.nVars()-1, false);
+    }
+
+    vector<Lit> var_sel(cnf.nVars());
+    for(uint32_t i = 0; i < cnf.nVars(); i++) {
+        s.new_var();
+        var_sel[i] = Lit(cnf.nVars()-1, false);
+    }
+
+    vector<Lit> cl;
+    for(uint32_t i = 0; i < cnf.get_clauses().size(); i++) {
+        const auto& ocl = cnf.get_clauses()[i];
+
+        //1. We replace every literal in the formula with a literal-substitute; x_j in the formula becomes x1_j , while ¬xj is replaced with x0_j .
+        //2. Each clause Ci is augmented with a clause-selector yi to form a new clause C′ i = (yi → Ci) = (¬yi ∨ Ci)
+        cl.clear();
+        for(const auto& l: ocl) {
+            if (l.sign()) cl.push_back(lit_sub[l.var()].neg);
+            else cl.push_back(lit_sub[l.var()].pos);
+        }
+        cl.push_back(cl_sel[i]);
+        s.add_clause(cl);
+
+
+        // We add clauses to require that a clause be enabled (yi = TRUE) if
+        // any one of its variables is enabled. Thus, for any xj present in clause Ci,
+        // we add a clause (x+_j → y_i) = (¬x+_j ∨ y_i).
+        for(const auto& l: ocl) {
+            s.add_clause({~var_sel[l.var()], cl_sel[i]});
+        }
+    }
+
+    /*
+    We create a variable-selector x+ j for every variable xj .
+    When x+ j is TRUE, xj will be enabled, and it is disabled otherwise.
+    For every variable xj , we add clauses to relate its variable-selector x+_j ,
+    its two literal-substitutes x0_j and x1_j , and the value of the variable itself, x_j .
+    In short, we want each literal- substitute to be TRUE when the variable is enabled (x+_j is TRUE)
+    and x_j has the corresponding value. This leads to new clauses encoding the following:
+    (x1 j = x+ j AND xj ) and (x0 j = x+ j AND ¬xj ).
+    */
+
+    for(uint32_t i = 0; i < cnf.nVars(); i++) {
+        // (x1 j = x+_j AND x_j)
+        cl.clear();
+        cl.push_back(lit_sub[i].pos);
+        cl.push_back(~var_sel[i]);
+        cl.push_back(~Lit(i, false));
+        s.add_clause(cl);
+        cl.clear();
+        cl.push_back(~lit_sub[i].pos);
+        cl.push_back(var_sel[i]);
+        s.add_clause(cl);
+        cl.clear();
+        cl.push_back(~lit_sub[i].pos);
+        cl.push_back(Lit(i, false));
+        s.add_clause(cl);
+
+        // (x0_j = x+_j AND ¬x_j)
+        cl.clear();
+        cl.push_back(lit_sub[i].neg);
+        cl.push_back(~var_sel[i]);
+        cl.push_back(~Lit(i, true));
+        s.add_clause(cl);
+        cl.clear();
+        cl.push_back(~lit_sub[i].neg);
+        cl.push_back(var_sel[i]);
+        s.add_clause(cl);
+        cl.clear();
+        cl.push_back(~lit_sub[i].neg);
+        cl.push_back(Lit(i, true));
+        s.add_clause(cl);
+    }
+
+
+}
---
+++ src/autarky.h
@@ -0,0 +1,54 @@
+/*
+ Arjun
+
+ Copyright (c) 2020, Mate Soos and Kuldeep S. Meel. All rights reserved.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+
+#pragma once
+
+#include <cryptominisat5/cryptominisat.h>
+#include "src/config.h"
+#include <vector>
+#include <set>
+#include "config.h"
+#include "arjun.h"
+
+using namespace CMSat;
+using namespace ArjunInt;
+using namespace ArjunNS;
+using std::vector;
+using std::set;
+
+class Autarky {
+public:
+    Autarky(const Config& _conf);
+
+    void do_autarky(SimplifiedCNF& cnf);
+
+private:
+    struct LitSub {
+        Lit pos = lit_Undef;
+        Lit neg = lit_Undef;
+    };
+    SATSolver s;
+    const Config conf;
+};
