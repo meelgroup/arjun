dedf9538c8d3768dd3d79dce87ebfedc464fa5ac
Mate Soos <soos.mate@gmail.com>
2026-03-05 21:48:33 +0100

    Fix cmake usage

--- scripts/build_norm.sh
+++ scripts/build_norm.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
-rm -rf lib* Test* tests* include tests CM* cmake* arjun Makefile rjun-src
+rm -rf .cmake lib* Test* tests* include tests CM* cmake* arjun Makefile rjun-src
 cmake -DENABLE_TESTING=ON ..
 make -j26
