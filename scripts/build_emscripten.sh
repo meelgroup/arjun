e49ce39d5da8ba86f714608fb280da0a4f7ef5a1
Mate Soos <soos.mate@gmail.com>
2026-03-07 11:17:11 +0100

    Update emscripten

--- scripts/build_emscripten.sh
+++ scripts/build_emscripten.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 rm -rf lib* Test* tests* include tests CM* cmake* arjun Makefile rjun-src
-emcmake cmake -DSTATICCOMPILE=ON "-DCMAKE_INSTALL_PREFIX=${EMINSTALL}" -DEXTRA_SYNTH=OFF ..
+emcmake cmake -DSTATICCOMPILE=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DSTATICCOMPILE=ON "-DCMAKE_INSTALL_PREFIX=${EMINSTALL}" -DEXTRA_SYNTH=OFF ..
 emmake make -j26
 emmake make install
 cp arjun.wasm ../html
