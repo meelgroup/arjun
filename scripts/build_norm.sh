6ad4a29f531d7bea5bdd2aab650d05be8759cf93
Mate Soos <soos.mate@gmail.com>
2026-04-04 16:38:57 +0200

    This fixes things
    Fix

--- scripts/build_norm.sh
+++ scripts/build_norm.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
 
 rm -rf .cmake
@@ -7,18 +8,21 @@ rm -rf Test*
 rm -rf tests*
 rm -rf include
 rm -rf tests
+rm -rf approxmc*
+rm -rf apx-src
 rm -rf CM*
 rm -rf cmake*
-rm -rf arjun
-rm -rf Makefile
-rm -rf rjun-src
 rm -rf deps
 rm -rf _deps
-cmake -DENABLE_TESTING=ON -DEXTRA_SYNTH=ON \
-    -Dcadical_DIR=../../cadical/build \
-    -Dcryptominisat5_DIR=../../cryptominisat/build \
-    -Dsbva_DIR=../../sbva/build \
-    -Dtreedecomp_DIR=../../treedecomp/build \
-    -DEvalMaxSAT_DIR=../../EvalMaxSAT/build \
+SOLVERS_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
+echo "solvers dir: $SOLVERS_DIR"
+cmake -DENABLE_TESTING=ON \
+    -Dcadical_DIR="${SOLVERS_DIR}/cadical/build" \
+    -Dcadiback_DIR="${SOLVERS_DIR}/cadiback/build" \
+    -Dcryptominisat5_DIR="${SOLVERS_DIR}/cryptominisat/build" \
+    -Dsbva_DIR="${SOLVERS_DIR}/sbva/build" \
+    -Dtreedecomp_DIR="${SOLVERS_DIR}/treedecomp/build" \
+    -Darjun_DIR="${SOLVERS_DIR}/arjun/build" \
     ..
 make -j$(nproc)
+make test
