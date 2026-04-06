44f661528b5e785718232ca54e400aea0ab29d98
Mate Soos <soos.mate@gmail.com>
2026-04-06 13:39:57 +0200

    Update arjun binary

--- scripts/build_static_release.sh
+++ scripts/build_static_release.sh
@@ -27,3 +27,13 @@ cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF \
     ..
 make -j$(nproc)
 strip arjun
+
+# Get SHA1 hashes and build versioned filename
+HASHES=$(./arjun -v 2>&1 || true)
+ARJUN_SHA=$(echo "$HASHES" | grep "Arjun SHA1" | grep -oP '[0-9a-f]{40}' | cut -c1-8)
+SBVA_SHA=$(echo "$HASHES" | grep "SBVA SHA1" | grep -oP '[0-9a-f]{40}' | cut -c1-8)
+CMS_SHA=$(echo "$HASHES" | grep "CMS SHA1" | grep -oP '[0-9a-f]{40}' | cut -c1-8)
+
+DEST="arjun_${ARJUN_SHA}_${SBVA_SHA}_${CMS_SHA}"
+cp arjun "$DEST"
+echo "Copied to $DEST"
