3ae94bf2467dd63038418c807e79fd0f7819df22
Mate Soos <soos.mate@gmail.com>
2026-04-19 12:44:49 +0200

    Build better

--- scripts/build_norm.sh
+++ scripts/build_norm.sh
@@ -8,8 +8,9 @@ rm -rf Test*
 rm -rf tests*
 rm -rf include
 rm -rf tests
-rm -rf approxmc*
-rm -rf apx-src
+rm -rf arjun
+rm -rf Makefile
+rm -rf src
 rm -rf CM*
 rm -rf cmake*
 rm -rf deps
@@ -17,12 +18,13 @@ rm -rf _deps
 SOLVERS_DIR="$(cd "$(dirname "$0")/../.." && pwd)"
 echo "solvers dir: $SOLVERS_DIR"
 cmake -DENABLE_TESTING=ON \
+    -DEXTRA_SYNTH=ON \
     -Dcadical_DIR="${SOLVERS_DIR}/cadical/build" \
     -Dcadiback_DIR="${SOLVERS_DIR}/cadiback/build" \
     -Dcryptominisat5_DIR="${SOLVERS_DIR}/cryptominisat/build" \
     -Dsbva_DIR="${SOLVERS_DIR}/sbva/build" \
     -Dtreedecomp_DIR="${SOLVERS_DIR}/treedecomp/build" \
-    -Darjun_DIR="${SOLVERS_DIR}/arjun/build" \
+    -DEvalMaxSAT_DIR="${SOLVERS_DIR}/EvalMaxSAT/build" \
     ..
 make -j$(nproc)
 make test
