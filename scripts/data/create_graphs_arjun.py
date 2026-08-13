85ad96b3765719bac3710a6dfbc997fe9a2c8661
Mate Soos <soos.mate@gmail.com>
2026-08-14 01:32:29 +0200

    Graph: add back old

--- scripts/data/create_graphs_arjun.py
+++ scripts/data/create_graphs_arjun.py
@@ -67,8 +67,10 @@ only_dirs = [
     # "out-synth-1945355-5", # vsids and some other checks
     # "out-synth-1945355-", # vsids and some other checks
     # "out-synth-1957159-2", # cleanup
-    "out-synth-1965186-0", # polarity that's easier?
+    # "out-synth-1965186-0", # polarity that's easier?
     "out-synth-1995325-0", # rnd strategy
+
+    "out-synth-1367674-0", # before unate-eq
 ]
 # -------------------------------------------------------------
 
