84c659f5f94af4a5859989a05ab02dbdc2e31c50
Mate Soos <soos.mate@gmail.com>
2026-09-03 12:15:51 +0200

    ADd new graph

--- scripts/data/create_graphs_arjun.py
+++ scripts/data/create_graphs_arjun.py
@@ -69,8 +69,9 @@ only_dirs = [
     # "out-synth-1957159-2", # cleanup
     # "out-synth-1965186-0", # polarity that's easier?
     "out-synth-1995325-0", # rnd strategy
+    "out-synth-2248210-0", # new CMS, new CaDiCaL
 
-    "out-synth-1367674-0", # before unate-eq
+    # "out-synth-1367674-0", # before unate-eq
 ]
 # -------------------------------------------------------------
 
