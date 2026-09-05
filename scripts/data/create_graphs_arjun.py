88f6e3a412aec41068dd55a477f7d908bc748aa3
Mate Soos <soos.mate@gmail.com>
2026-09-05 22:43:21 +0200

    Add new graph

--- scripts/data/create_graphs_arjun.py
+++ scripts/data/create_graphs_arjun.py
@@ -69,8 +69,9 @@ only_dirs = [
     # "out-synth-1957159-2", # cleanup
     # "out-synth-1965186-0", # polarity that's easier?
     "out-synth-1995325-0", # rnd strategy
-    "out-synth-2248210-0", # new CMS, new CaDiCaL
+    # "out-synth-2248210-0", # new CMS, new CaDiCaL
     # "out-synth-2253327-0", # stupid idea about unate + no-unate
+    "out-synth-2261154-0", # "develop" CMS, but with CaDiCaL 3.0.1 lucky issue fixed
 
     # "out-synth-1367674-0", # before unate-eq
 ]
