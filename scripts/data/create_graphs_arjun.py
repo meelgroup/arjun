2bc1f195ca3828a6c93edd0198f1e3591c82d934
Mate Soos <soos.mate@gmail.com>
2026-09-08 00:23:36 +0200

    Graph

--- scripts/data/create_graphs_arjun.py
+++ scripts/data/create_graphs_arjun.py
@@ -72,8 +72,9 @@ only_dirs = [
     # "out-synth-2248210-0", # new CMS, new CaDiCaL
     # "out-synth-2253327-0", # stupid idea about unate + no-unate
     # "out-synth-2261154-0", # "develop" CMS, but with CaDiCaL 3.0.1 lucky issue fixed
-    "out-synth-2263789-0", # fixing up the distill with new cadical
+    # "out-synth-2263789-0", # fixing up the distill with new cadical
     "out-synth-2263789-2", # fixing up the distill with new cadical
+    "out-synth-2272478-4", # even better maybe
 
     # "out-synth-1367674-0", # before unate-eq
 ]
